d83b33118ccf5ada0e69996b6d2c79111465bea8
Divyank <divyank25597@gmail.com>
2025-09-08 16:24:05 +0530

    Remoce duplicate frontend files from backend

--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/formulate-borrow-request.ts';
-import '@/ai/flows/formulate-lend-request.ts';
--- src/components/icons.tsx
+++
@@ -1,6 +0,0 @@
-import { Leaf } from "lucide-react";
-import type { LucideProps } from "lucide-react";
-
-export const Icons = {
-  logo: (props: LucideProps) => <Leaf {...props} />,
-};
